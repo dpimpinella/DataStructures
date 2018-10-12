0df884c3786a680a9d5d0eded0f4265496a9416d
Dominic Pimpinella <dom.pimpinella@gmail.com>
2018-10-12 13:52:26 -0400

    Overloaded +,-,<,>, and added print/display functions

--- Week4/MoneyWithOverloading/MoneyWithOverloading/money_with_overloading.cpp
+++ Week4/MoneyWithOverloading/MoneyWithOverloading/money_with_overloading.cpp
@@ -5,60 +5,104 @@
 
 
 #include <iostream>
+#include <iomanip>
 #include <cmath>
 
 class Money{
 private:
-  int dollars_;
   int cents_;
-  int DollarsPart(double);
-  int CentsPart(double);
   
 public:
   Money(){
-    this->dollars_ = 0;
     this->cents_ = 0;
   };
   
   Money(double amount){
-    this->dollars_ = DollarsPart(amount);
-    this->cents_ = CentsPart(amount);
+    this->cents_ = amount * 100;
   };
   
   Money(int dollars, int cents){
-    this->dollars_ = dollars;
-    this->cents_ = cents;
+    this->cents_ = dollars * 100 + cents;
   };
   
   Money(int dollars){
-    this->dollars_ = dollars;
-    this->cents_ = 0;
+    this->cents_ = dollars * 100;
   };
-  
+  Money Add(Money&);
+  Money operator+(Money&);
+  Money Subtract(Money&);
+  Money operator-(Money&);
+  bool GreaterThan(Money&);
+  bool operator>(Money&);
+  bool LessThan(Money&);
+  bool operator<(Money&);
   void DisplayPercentage(double);
+  double FormatCentsAsDollars(int);
+  void PrintMoney();
 };
 
-int Money::DollarsPart(double amount){
-  return static_cast<int>(amount);
+Money Money::Add(Money& other){
+  double dollars_and_cents = (this->cents_ + other.cents_) / 100;
+  Money sum = Money(dollars_and_cents);
+  return sum;
 }
 
-int Money::CentsPart(double amount){
-  double double_cents = amount * 100;
-  int int_cents = int(round(fabs(double_cents))) % 100;
-  if (amount < 0){
-    int_cents = -int_cents;
-  }
-    return int_cents;
+Money Money::operator+(Money& other){
+  return Add(other);
 }
+
+Money Money::Subtract(Money& other){
+  double dollars_and_cents = (this->cents_ - other.cents_) / 100;
+  Money difference = Money(dollars_and_cents);
+  return difference;
+}
+
+Money Money::operator-(Money& other){
+  return Subtract(other);
+}
+
+bool Money::GreaterThan(Money& other){
+  bool answer = this->cents_ > other.cents_;
+  return answer;
+}
+
+bool Money::operator>(Money& other){
+  return GreaterThan(other);
+}
+
+bool Money::LessThan(Money& other){
+  bool answer = this->cents_ < other.cents_;
+  return answer;
+}
+
+bool Money::operator<(Money& other){
+  return LessThan(other);
+}
+
+double Money::FormatCentsAsDollars(int amount){
+  double amount_to_display = (double)amount / 100;
+  return amount_to_display;
+}
+
 
 void Money::DisplayPercentage(double percentage){
   double decimal_percentage = percentage / 100;
-  double amount = ((double)dollars_ + (double)cents_ / 100) * decimal_percentage;
-  std::cout << percentage << "% of your money is:  $" << amount << '\n';
+  int percent_of_total = (int)(decimal_percentage * cents_);
+  FormatCentsAsDollars(percent_of_total);
+}
+
+void Money::PrintMoney(){
+  double amount_to_display = FormatCentsAsDollars(this->cents_);
+  std::cout << std::fixed;
+  std::cout << std::setprecision(2);
+  std::cout << "$" << amount_to_display << "\n";
+
 }
 
 int main(){
-  Money money = Money(10);
-  money.DisplayPercentage(33);
+  Money money1 = Money(100);
+  Money money2 = Money(200);
+  bool answer = money2 < money1;
+  std::cout << answer;
   return 0;
 }
